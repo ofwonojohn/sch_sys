29425cf0249daef0e147e4e99bd150a4f23ec8b6
Ofwonojohn <Ofwonojohn004@gmail.com>
2026-02-14 17:19:41 +0300

    created a maven springboot project for a primary school management system

---
+++ primary_system/primaryschool/src/main/java/primaryschool/PrimaryschoolApplication.java
@@ -0,0 +1,13 @@
+package primaryschool;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PrimaryschoolApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PrimaryschoolApplication.class, args);
+	}
+
+}
